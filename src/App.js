1314a2bcbe9a95cf39918a8486f26a1b57ff2b33
Антон <loparuxin.a@gmail.com>
2023-11-10 19:53:44 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <NavBar />
+            <ul>
+                <li>css</li>
+                <li>crp</li>
+                <li>rpg</li>
+                <li>html</li>
+            </ul>
+        </div>
+    );
 }
 
+const NavBar = () => {
+    return (
+        <div>
+            <a href='/'>Форум</a>
+            <a href='/'>Сообщения</a>
+            <a href='/'>Настройки</a>
+        </div>
+    );
+}
 export default App;
