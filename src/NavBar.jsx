ef398e5fcf00fc4b10bb322fcf83c19ce4ca2cc7
Антон <loparuxin.a@gmail.com>
2023-11-13 17:42:20 +0300

    доработка верстки navBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,9 +2,9 @@ import navBar from './NavBar.module.css'
 export const NavBar = () => {
     return (
         <nav className={navBar.navBar}>
-            <div>Профиль</div>
-            <div>Курс</div>
-            <div>Сообщество</div>
+            <div className={navBar.profile}>Профиль</div>
+            <div className={navBar.course}>Курс</div>
+            <div className={navBar.community}>Сообщество</div>
         </nav>
     )
 }
