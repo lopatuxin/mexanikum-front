5ffc3d266a9f406b9a377e56cb0c42c35ff482c1
Антон <loparuxin.a@gmail.com>
2023-11-16 20:09:23 +0300

    Реализовал при клике на сообщение переход на другой адрес

--- src/components/content/profile/messages/Messages.jsx
+++ src/components/content/profile/messages/Messages.jsx
@@ -1,12 +1,13 @@
 import messages from './Messages.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Messages = () => {
     return (
         <div className={messages.messages}>
-            <div className={messages.message}>
+            <NavLink to='/messages/1' className={messages.message}>
                 <div className={messages.avatar}>
                     <img className={messages.avatar}
-                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'/>
+                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg' alt='img'/>
                 </div>
                 <div className={messages.name}>
                     <h4 className={messages.name}>Антон Лопатухин</h4>
@@ -14,11 +15,11 @@ export const Messages = () => {
                 <div className={messages.body}>
                     <p className={messages.body}>Тело</p>
                 </div>
-            </div>
-            <div className={messages.message}>
+            </NavLink>
+            <NavLink to='/messages/2' className={messages.message}>
                 <div className={messages.avatar}>
                     <img className={messages.avatar}
-                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'/>
+                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg' alt='img'/>
                 </div>
                 <div className={messages.name}>
                     <h4 className={messages.name}>Антон Лопатухин</h4>
@@ -26,7 +27,7 @@ export const Messages = () => {
                 <div className={messages.body}>
                     <p className={messages.body}>Тело</p>
                 </div>
-            </div>
+            </NavLink>
         </div>
     )
 }
