22e6307819918f5ece4d73bd0473cadd116db336
Антон <loparuxin.a@gmail.com>
2023-11-16 12:02:00 +0300

    Структура страницы Messages и верстка для нее

--- src/components/content/profile/messages/Messages.jsx
+++ src/components/content/profile/messages/Messages.jsx
@@ -1,6 +1,32 @@
 import messages from './Messages.module.css'
+
 export const Messages = () => {
     return (
-        <div className={messages.messages}>Сообщения</div>
+        <div className={messages.messages}>
+            <div className={messages.message}>
+                <div className={messages.avatar}>
+                    <img className={messages.avatar}
+                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'/>
+                </div>
+                <div className={messages.name}>
+                    <h4 className={messages.name}>Антон Лопатухин</h4>
+                </div>
+                <div className={messages.body}>
+                    <p className={messages.body}>Тело</p>
+                </div>
+            </div>
+            <div className={messages.message}>
+                <div className={messages.avatar}>
+                    <img className={messages.avatar}
+                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'/>
+                </div>
+                <div className={messages.name}>
+                    <h4 className={messages.name}>Антон Лопатухин</h4>
+                </div>
+                <div className={messages.body}>
+                    <p className={messages.body}>Тело</p>
+                </div>
+            </div>
+        </div>
     )
 }
