6e23c03702d7afd7f36ed2b80aa83bbb5b22fc2b
Антон <loparuxin.a@gmail.com>
2023-11-17 12:12:46 +0300

    Выношу данные за пределы компонентов и не большие доработки верстки

--- src/components/content/profile/messages/Messages.jsx
+++ src/components/content/profile/messages/Messages.jsx
@@ -1,32 +1,21 @@
 import messages from './Messages.module.css'
+import {Message} from "./message/Message";
 import {NavLink} from "react-router-dom";
 
 export const Messages = () => {
+    let dialogs = [
+        {name : 'Антон Лопатухин', message : 'Привет'},
+        {name : 'Владимир Лопатухин', message : 'Привет, как дела'},
+        {name : 'Елена Лопатухина', message : 'Пока'},
+        {name : 'Леля Лопатухина', message : 'Привет'},
+        {name : 'Антон Лопатухин', message : 'Привет'}
+    ]
+    let dialog = dialogs.map(message =>
+        <Message name={message.name} message={message.message}/>)
     return (
         <div className={messages.messages}>
-            <NavLink to='/messages/1' className={messages.message}>
-                <div className={messages.avatar}>
-                    <img className={messages.avatar}
-                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg' alt='img'/>
-                </div>
-                <div className={messages.name}>
-                    <h4 className={messages.name}>Антон Лопатухин</h4>
-                </div>
-                <div className={messages.body}>
-                    <p className={messages.body}>Тело</p>
-                </div>
-            </NavLink>
-            <NavLink to='/messages/2' className={messages.message}>
-                <div className={messages.avatar}>
-                    <img className={messages.avatar}
-                         src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg' alt='img'/>
-                </div>
-                <div className={messages.name}>
-                    <h4 className={messages.name}>Антон Лопатухин</h4>
-                </div>
-                <div className={messages.body}>
-                    <p className={messages.body}>Тело</p>
-                </div>
+            <NavLink to='/messages/1' className={messages.messages}>
+                {dialog}
             </NavLink>
         </div>
     )
